ea986302c6a4c884597721e708daddedd41482d6
oriamram <oriamram111@gmail.com>
2025-05-30 11:56:18 +0300

    added header

---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import logo from "@/assets/logo.png";
+import { ScrollText } from "lucide-vue-next";
+import SearchBar from "@/components/SearchBar.vue";
+import Button from "./ui/button/Button.vue";
+</script>
+
+<template>
+	<header class="w-full">
+		<div class="flex items-center justify-between">
+			<!-- Logo -->
+			<a href="https://cyvore.com" target="_blank" rel="noopener noreferrer" class="flex items-center cursor-pointer">
+				<img :src="logo" alt="Cyvore Logo" class="h-auto w-32 mr-3" />
+			</a>
+			<!-- Search Bar -->
+			<SearchBar />
+
+			<!-- Logs Button -->
+			<Button variant="secondary" size="icon" class="size-12">
+				<ScrollText :size="30" class="text-neutral-100" />
+			</Button>
+		</div>
+	</header>
+</template>
---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup>
+import Header from "@/components/Header.vue";
+</script>
+
+<template>
+	<div class="flex flex-col items-center h-full w-full rounded-lg shadow-lg bg-white/20 p-6">
+		<Header />
+	</div>
+</template>
