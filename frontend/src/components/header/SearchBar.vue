6a1e393fae433e0a17e0fda73f66ea031207944a
oriamram <oriamram111@gmail.com>
2025-06-03 10:45:59 +0300

    updated ignore

--- frontend/src/components/header/SearchBar.vue
+++ frontend/src/components/header/SearchBar.vue
@@ -28,7 +28,7 @@ const handleScanClick = async () => {
 <template>
 	<div class="bg-white rounded-full flex items-center p-1 gap-2 md:gap-3 w-full md:w-1/2 lg:w-1/3">
 		<Input class="rounded-full border-none !ring-0 font-medium !text-base md:!text-xl shadow-none" type="text" v-model="searchValue" placeholder="cyvore.com" />
-		<Button class="rounded-full w-8 h-8 md:w-10 md:h-10 bg-primary" @click="handleScanClick">
+		<Button class="rounded-full w-8 h-8 md:w-10 md:h-10 bg-green-500" @click="handleScanClick">
 			<Eye class="text-white -scale-x-[100%] w-4 h-4 md:w-5 md:h-5" />
 		</Button>
 	</div>
